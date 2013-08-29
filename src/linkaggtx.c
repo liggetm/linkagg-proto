1856fafc7e77786b9b8cb88569dc02352aeca381
ml2387 <ml2387@my.open.ac.uk>
2013-08-29 01:54:41 +0100

    Modified linkaggtx to use a hardcoded array of tx rings. Currently just iterates across the 2 hardcoded interfaces.

--- src/linkaggtx.c
+++ src/linkaggtx.c
@@ -135,16 +135,13 @@ int read_packet_hex(u_char *buf, int buf_len) {
 /* *************************************** */
 
 void print_stats() {
-  double deltaMillisec, currentThpt, avgThpt, currentThptBits, currentThptBytes, avgThptBits, avgThptBytes;
+  double deltaMillisec, avgThpt, avgThptBits, avgThptBytes;
   struct timeval now;
   char buf1[64], buf2[64], buf3[64], buf4[64], buf5[64], statsBuf[512], timebuf[128];
   u_int64_t deltaMillisecStart;
 
   gettimeofday(&now, NULL);
   deltaMillisec = delta_time(&now, &lastTime);
-  currentThpt = (double)((num_pkt_good_sent-last_num_pkt_good_sent) * 1000)/deltaMillisec;
-  currentThptBytes = (double)((num_bytes_good_sent-last_num_bytes_good_sent) * 1000)/deltaMillisec;
-  currentThptBits = currentThptBytes * 8;
 
   deltaMillisec = delta_time(&now, &startTime);
   avgThpt = (double)(num_pkt_good_sent * 1000)/deltaMillisec;
@@ -152,9 +149,7 @@ void print_stats() {
   avgThptBits = avgThptBytes * 8;
 
   snprintf(statsBuf, sizeof(statsBuf),
-	   "TX rate: [current %s pps/%s Gbps][average %s pps/%s Gbps][total %s pkts]",
-	   pfring_format_numbers(currentThpt, buf1, sizeof(buf1), 1),
-	   pfring_format_numbers(currentThptBits/(1000*1000*1000), buf2, sizeof(buf2), 1),
+	   "TX rate: [average %s pps/%s Gbps][total %s pkts]",
 	   pfring_format_numbers(avgThpt, buf3, sizeof(buf3), 1),
 	   pfring_format_numbers(avgThptBits/(1000*1000*1000),  buf4, sizeof(buf4), 1),
 	   pfring_format_numbers(num_pkt_good_sent, buf5, sizeof(buf5), 1));
@@ -165,14 +160,10 @@ void print_stats() {
   snprintf(statsBuf, sizeof(statsBuf),
            "Duration:          %s\n"
            "SentPackets:       %lu\n"
-           "SentBytes:         %lu\n"
-           "CurrentSentPps:    %lu\n"
-           "CurrentSentBitps:  %lu\n",
+           "SentBytes:         %lu\n",
            sec2dhms((deltaMillisecStart/1000), timebuf, sizeof(timebuf)),
            (long unsigned int) num_pkt_good_sent,
-           (long unsigned int) num_bytes_good_sent,
-	   (long unsigned int) currentThpt,
-	   (long unsigned int) currentThptBits);
+           (long unsigned int) num_bytes_good_sent);
   pfring_set_application_stats(pd, statsBuf);
 
   memcpy(&lastTime, &now, sizeof(now));
@@ -205,28 +196,14 @@ void sigproc(int sig) {
 /* *************************************** */
 
 void printHelp(void) {
-  printf("pfsend - (C) 2011-13 ntop.org\n");
-  printf("Replay synthetic traffic, or a pcap, or a packet in hex format from standard input.\n\n"); 
-  printf("pfsend -i out_dev [-a] [-f <.pcap file>] [-g <core_id>] [-h]\n"
-         "       [-l <length>] [-n <num>][-r <rate>] [-m <dst MAC>]\n"
-	 "       [-w <TX watermark>] [-v]\n\n");
-  printf("-a              Active send retry\n");
-#if 0
-  printf("-b <cpu %%>      CPU pergentage priority (0-99)\n");
-#endif
-  printf("-f <.pcap file> Send packets as read from a pcap file\n");
-  printf("-g <core_id>    Bind this app to a core\n");
+  printf("linkaggtx\n");
+  printf("Hacked version of pfsend - allows a list of interfaces.\n\n");
+  printf("linkaggtx -i out_dev [-h]\n"
+         "       [-l <length>] [-n <num>]\n\n");
   printf("-h              Print this help\n");
   printf("-i <device>     Device name. Use device\n");
   printf("-l <length>     Packet length to send. Ignored with -f\n");
   printf("-n <num>        Num pkts to send (use 0 for infinite)\n");
-  printf("-r <rate>       Rate to send (example -r 2.5 sends 2.5 Gbit/sec, -r -1 pcap capture rate)\n");
-  printf("-m <dst MAC>    Reforge destination MAC (format AA:BB:CC:DD:EE:FF)\n");
-  printf("-b <num>        Number of different IPs (balanced traffic)\n");
-  printf("-w <watermark>  TX watermark (low value=low latency) [not effective on DNA]\n");
-  printf("-z              Disable zero-copy, if supported [DNA only]\n");
-  printf("-x <if index>   Send to the selected interface, if supported\n");
-  printf("-v              Verbose\n");
   exit(0);
 }
 
@@ -323,14 +300,10 @@ static void forge_udp_packet(u_char *buffer, u_int buffer_len, u_int idx) {
 /* *************************************** */
 
 int main(int argc, char* argv[]) {
-  char path[255] = { 0 };
-  int c, i, verbose = 0, active_poll = 0;
+  int c, i = 0;
   u_char buffer[9000];
   u_int32_t num_to_send = 0;
-  double gbit_s = 0;
-  ticks tick_start = 0, tick_delta = 0;
   struct packet *tosend;
-  u_int num_tx_slots = 0;
 
   while((c = getopt(argc, argv, "hi:j:n:l:")) != -1) {
     switch(c) {
@@ -358,30 +331,29 @@ int main(int argc, char* argv[]) {
   if((in_dev == NULL) || (optind < argc)) /* Extra argument */
     printHelp();
 
-  printf("Sending packets on %s\n", in_dev);
-
-  pd = pfring_open(in_dev, 1500, 0 /* PF_RING_PROMISC */);
-  if(pd == NULL) {
-    printf("pfring_open %s error [%s]\n", in_dev, strerror(errno));
-    return(-1);
-  } else {
-    u_int32_t version;
-
-    pfring_set_application_name(pd, "linkaggtx");
-    pfring_version(pd, &version);
-
-    printf("Using PF_RING v.%d.%d.%d\n", (version & 0xFFFF0000) >> 16,
-	   (version & 0x0000FF00) >> 8, version & 0x000000FF);
-  }
 
   /*
-   * Other j device if present
+   * Open and enable the list of devices
    */
-  if(in_devother != NULL) {
-	  pdother = pfring_open(in_devother, 1500, 0 /* PF_RING_PROMISC */);
-	  if(pdother == NULL) {
-	    printf("pfring_open %s error [%s]\n", in_devother, strerror(errno));
-	    return(-1);
+  char* deviceNames[2] = { "eth1", "eth2" };	/* TODO: remove hardcoded devices */
+  pfring* activeRings[2];
+  int m = 0;
+  for(m=0; m < sizeof(deviceNames)/sizeof(deviceNames[0]); m++) {
+	  pfring *ring = pfring_open(deviceNames[m], 1500, 0);
+	  if(ring == NULL) {
+		  printf("pfring_open %s error [%s]\n", deviceNames[m], strerror(errno));
+		  return(-1);
+	  } else {
+		  printf("Sending packets on %s\n", deviceNames[m]);
+		  pfring_set_socket_mode(ring, send_only_mode);
+
+		  if(pfring_enable_ring(ring) != 0) {
+		    printf("Unable to enable ring :-(\n");
+		    pfring_close(ring);
+		    return(-1);
+		  } else {
+			  activeRings[m] = ring;
+		  }
 	  }
   }
 
@@ -399,13 +371,10 @@ int main(int argc, char* argv[]) {
 	  send_len = stdin_packet_len;
   }
 
-
   if (stdin_packet_len <= 0) {
 	  printf("Reforging udp packet\n");
 	  forge_udp_packet(buffer, sizeof(buffer), i);
   }
-
-  /* TODO else: reforge IP only */
 
   p = (struct packet *) malloc(sizeof(struct packet));
   if(p) {
@@ -427,58 +396,32 @@ int main(int argc, char* argv[]) {
   } else {
 	  /* oops, couldn't allocate memory */
 	  fprintf(stderr, "Unable to allocate memory requested (%s)\n", strerror(errno));
-	  return (-1);
-  }
-
-  if(!verbose) {
-    signal(SIGALRM, my_sigalarm);
-    alarm(1);
   }
 
   gettimeofday(&startTime, NULL);
   memcpy(&lastTime, &startTime, sizeof(startTime));
 
-  pfring_set_socket_mode(pd, send_only_mode);
-  if(pdother != NULL)
-	  pfring_set_socket_mode(pdother, send_only_mode);
-
-  if(pfring_enable_ring(pd) != 0) {
-    printf("Unable to enable ring :-(\n");
-    pfring_close(pd);
-    return(-1);
-  }
-
-  if(pfring_enable_ring(pdother) != 0) {
-    printf("Unable to enable ring :-(\n");
-    pfring_close(pd);
-    return(-1);
-  }
-
   tosend = pkt_head;
   i = 0;
-
-  pfring_set_application_stats(pd, "Statistics not yet computed: please try again...");
-  if(pfring_get_appl_stats_file_name(pd, path, sizeof(path)) != NULL)
-    fprintf(stderr, "Dumping statistics on %s\n", path);
+  int numOfDevices = sizeof(deviceNames)/sizeof(deviceNames[0]);
+  printf("No. of active devices: %u\n", numOfDevices);
+  int deviceIdx = 0;
 
   while((num_to_send == 0) || (i < num_to_send)) {
-    int idev_rc, jdev_rc;
+    int rc;
 
   redo:
 
-  idev_rc = pfring_send(pd, tosend->pkt, tosend->len, gbit_s < 0 ? 1 : 0 /* Don't flush (it does PF_RING automatically) */);
-  jdev_rc = pfring_send(pdother, tosend->pkt, tosend->len, gbit_s < 0 ? 1 : 0 /* Don't flush (it does PF_RING automatically) */);
+  rc = pfring_send(activeRings[deviceIdx], tosend->pkt, tosend->len, 0);
 
-    if((idev_rc == PF_RING_ERROR_INVALID_ARGUMENT) || (idev_rc == PF_RING_ERROR_INVALID_ARGUMENT)){
+    if(rc == PF_RING_ERROR_INVALID_ARGUMENT){
       printf("Attempting to send invalid packet [len: %u][MTU: %u]%s\n",
 	     tosend->len, pd->mtu_len,
       	     if_index != -1 ? " or using a wrong interface id" : "");
-    } else if((idev_rc < 0) || (jdev_rc < 0)){
-      /* Not enough space in buffer */
-    	if(!active_poll) {
-    		usleep(1);
-    	}
-      goto redo;
+    } else if(rc < 0) {
+    	/* Not enough space in buffer */
+    	usleep(1);
+    	goto redo;
     }
 
     num_pkt_good_sent++;
@@ -486,25 +429,21 @@ int main(int argc, char* argv[]) {
 
     tosend = tosend->next;
 
-    if(num_pkt_good_sent == num_tx_slots)
-      tosend = pkt_head;
+    tosend = pkt_head;
 
-    if(gbit_s > 0) {
-      /* rate set */
-      while((getticks() - tick_start) < (num_pkt_good_sent * tick_delta)) ;
-    } else if (gbit_s < 0) {
-      /* real pcap rate */
-      if (tosend->ticks_from_beginning == 0)
-        tick_start = getticks(); /* first packet, resetting time */
-      while((getticks() - tick_start) < tosend->ticks_from_beginning) ;
-    }
-
-    if(num_to_send > 0) i++;
-  } /* for */
+    if(num_to_send > 0)
+    	i++;
+    deviceIdx++;
+    if(deviceIdx == numOfDevices)
+    	deviceIdx = 0;
+  }
 
   print_stats(0);
-  pfring_close(pd);
-  pfring_close(pdother);
+
+  int k = 0;
+  for(k=0; k < sizeof(activeRings)/sizeof(activeRings[0]); k++) {
+	  pfring_close(activeRings[k]);
+  }
 
   return(0);
 }
